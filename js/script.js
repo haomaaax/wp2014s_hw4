8da8acbee0d94b9bcc1fe8d3bed21e9e1078d1e6
Max Chen <maxyay5566@gmail.com>
2014-06-10 00:55:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ FB.getLoginStatus(function(response) {
 			//canvas.height = profileIMG.height;
 			canvas.width = 560;
 			canvas.height = 560;
-			ctx.drawImage(profileIMG,canMouseX-100/2,canMouseY-100/2);//從XY軸0，0值開始畫如profileimg
+			ctx.drawImage(profileIMG,canMouseX+50/2,canMouseY+50/2);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-50/2,canMouseY-50/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
