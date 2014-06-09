4a302ca9d98592ab7461302374d43a3f6f0cd2ed
Max Chen <maxyay5566@gmail.com>
2014-06-10 00:46:06 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ FB.getLoginStatus(function(response) {
 	  var img = new Image(); // 新增圖像1
 	 img.src = "img/overlay2.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2
-	img2.src = "img/overlayback.png" //圖像路徑
+	img2.src = "img/overlay2.png" //圖像路徑
 	var img3 = new Image();//新增圖像3
 	img3.src = "img/typography.png"//圖像路徑
 	
@@ -104,12 +104,12 @@ FB.getLoginStatus(function(response) {
 			canvas.width = 560;
 			canvas.height = 560;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
-			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+			ctx.drawImage(img3,canMouseX-50/2,canMouseY-50/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
-			ctx.fillText(inputedText, 0, 0); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+			ctx.fillText(inputedText, 280, 280); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
       }
     }
 
