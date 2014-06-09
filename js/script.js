2f04239ee6659a6735e8456c82e71906fb5ac8f9
Max Chen <maxyay5566@gmail.com>
2014-06-10 00:41:11 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ FB.getLoginStatus(function(response) {
 	ctx.font='20px "Arial"'; //設定字體與大小
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
 	  var img = new Image(); // 新增圖像1
-	 img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
+	 img.src = "img/overlay2.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2
 	img2.src = "img/overlayback.png" //圖像路徑
 	var img3 = new Image();//新增圖像3
@@ -101,8 +101,8 @@ FB.getLoginStatus(function(response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			//canvas.height = profileIMG.height;
-			canvas.width = 550;
-			canvas.height = 550;
+			canvas.width = 560;
+			canvas.height = 560;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
